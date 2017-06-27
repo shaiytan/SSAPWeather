5b2082bf22ba72d46045df2bdb07b5b99ecc612b
Shaiytan <postscriptum105@gmail.com>
2017-06-27 05:57:29 +0300

    Reorganize MainActivity

--- app/src/main/java/shaiytan/ssapweather/view/MainActivity.java
+++ app/src/main/java/shaiytan/ssapweather/view/MainActivity.java
@@ -1,21 +1,24 @@
-package shaiytan.ssapweather;
+package shaiytan.ssapweather.view;
 
 import android.app.PendingIntent;
 import android.content.*;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.*;
+import android.view.View;
 import android.widget.*;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.*;
 
+import shaiytan.ssapweather.R;
 import shaiytan.ssapweather.content.*;
 import shaiytan.ssapweather.service.WeatherService;
-import shaiytan.ssapweather.view.WeatherAdapter;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     private DBHelper dbHelper;
@@ -26,11 +29,37 @@ public class MainActivity extends AppCompatActivity {
     private RecyclerView forecastView;
     private SwitchCompat forecastSwitch;
     private SwipeRefreshLayout swipeUpdater;
+    private FloatingActionButton btnMap;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init();
+        Cursor forecast = dbHelper.readForecast(forecastSwitch.isChecked());
+        if(forecast.getCount()>0) {
+            WeatherItem currentWeather = dbHelper.readWeather();
+            long lastUpdate = currentWeather.getDatetime()*1000;
+            if(System.currentTimeMillis()-lastUpdate<30*60*1000) {
+                setWeatherView(currentWeather);
+                setForecastView(forecast);
+            }
+            else {
+                Intent intent = new Intent(this, WeatherService.class);
+                invokeService(intent);
+            }
+        }
+        else {
+            Intent intent = new Intent(this, WeatherService.class);
+            invokeService(intent);
+        }
+    }
+    private void init()
+    {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        btnMap = (FloatingActionButton) findViewById(R.id.map_btn);
         icon = (ImageView) findViewById(R.id.ic_weather);
         desc = (TextView) findViewById(R.id.desc);
         temp = (TextView) findViewById(R.id.temp);
@@ -57,23 +86,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         dbHelper = new DBHelper(this);
-        Cursor forecast = dbHelper.readForecast(forecastSwitch.isChecked());
-        if(forecast.getCount()>0) {
-            WeatherItem currentWeather = dbHelper.readWeather();
-            long lastUpdate = currentWeather.getDatetime()*1000;
-            if(System.currentTimeMillis()-lastUpdate<30*60*1000) {
-                setWeatherView(currentWeather);
-                setForecastView(forecast);
-            }
-            else {
-                Intent intent = new Intent(this, WeatherService.class);
-                invokeService(intent);
-            }
-        }
-        else {
-            Intent intent = new Intent(this, WeatherService.class);
-            invokeService(intent);
-        }
     }
     private void invokeService(Intent intent)
     {
@@ -115,4 +127,6 @@ public class MainActivity extends AppCompatActivity {
         swipeUpdater.setRefreshing(false);
     }
 
+    public void onMapClick(View view) {
+    }
 }
